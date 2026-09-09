f8990f5ba15a6bcd327b3f45fa4f94ad7b03639d
gustavo <crt.gustavo@gmail.com>
2026-09-09 10:35:48 -0300

    usar PORT de env para produccion

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import { PrismaClient } from '@prisma/client';
 
 const app = express();
 const prisma = new PrismaClient();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
